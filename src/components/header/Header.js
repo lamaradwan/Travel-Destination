d70e60138ca72301027ad5316a7542c7cebd7018
Lama Radwan <lama.radwan96@gmail.com>
2022-05-11 22:23:23 +0300

    Created a Tour, Home, Footer, Header components

---
+++ src/components/header/Header.js
@@ -0,0 +1,8 @@
+function Header(){
+    return(
+    <>
+    <h1 style={{backgroundColor:'pink'}}>This is the tours website header, welcome ..</h1>
+    </>
+    )
+}
+export default Header;
---
+++ src/components/tours/Tours.js
@@ -0,0 +1,10 @@
+function Tours(props){
+return(
+    <div>
+        <h3>{props.countryName}</h3>
+        <img src={props.image}/>
+        <hr/>
+    </div>
+)
+}
+export default Tours;
